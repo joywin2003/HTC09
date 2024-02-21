c990c95d6848e6127b6305d39e8a4ccf16fd2ae1
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-02-21 08:21:08 +0530

    user and status

--- src/app/dasboard/page.tsx
+++ src/app/dasboard/page.tsx
@@ -22,49 +22,6 @@ export default function Overview() {
       <div className="grid place-items-center pb-10 font-bold">
         <h1 className="text-5xl flex items-center">Analytics Page</h1>
       </div>
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 pb-10">
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">No of users</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">$45,231.89</div>
-            <p className="text-xs text-muted-foreground">
-              +20.1% from last month
-            </p>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">DDOS attack mode</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">$45,231.89</div>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">DDOS attempts(24hr)</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">$45,231.89</div>
-            <p className="text-xs text-muted-foreground">
-              +9.1% from last month
-            </p>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">SQL injection attempts(24hr)</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">$45,231.89</div>
-            <p className="text-xs text-muted-foreground">
-              +0.8% from last month
-            </p>
-          </CardContent>
-        </Card>
-      </div>
       {/* <ResponsiveContainer width="100%" height={350} className="pb-10">
         <BarChart data={data}>
           <XAxis
--- src/app/ui/analytic.tsx
+++ src/app/ui/analytic.tsx
@@ -22,18 +22,18 @@ import { themes } from "@/registry/themes";
 import { useState, useEffect } from "react";
 import { set } from "react-hook-form";
 
-
 // interface DataItem {
 //     time: string;
 //     value: number;
 //   }
-  
+
 export function CardsMetric() {
   const { theme: mode } = useTheme();
   const [config] = useConfig();
-  const [data, setData] = useState([
-  ]);
-    // let data: DataItem[] = [];
+  const [data, setData] = useState([]);
+  const [user, setUser] = useState(0);
+  const [status, setStatus] = useState("NORMAL");
+  // let data: DataItem[] = [];
   const theme = themes.find((theme) => theme.name === config.theme);
 
   // Define fetchData function
@@ -57,12 +57,10 @@ export function CardsMetric() {
         }
 
         const response_data = await response.json();
-        console.log(response_data["value"]);
+        console.log(response_data);
         setData(response_data["value"]);
-        console.log(typeof data);
-        console.log(0, data);
-        console.log(typeof data);
-        console.log(1, data);
+        setUser(response_data["TOT_USERS"]);
+        setStatus(response_data["MODE"]);
       } catch (error) {
         console.error("Error posting data:", error);
       }
@@ -77,71 +75,97 @@ export function CardsMetric() {
   }, [data]);
 
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Network usage coverage</CardTitle>
-        <CardDescription>
-          Your exercise minutes are ahead of where you normally are.
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="pb-4">
-        <div className="h-[200px]">
-          <ResponsiveContainer width="100%" height="100%">
-            <LineChart
-              data={data}
-              margin={{
-                top: 5,
-                right: 10,
-                left: 10,
-                bottom: 0,
-              }}
-            >
-              <XAxis dataKey="time" />
-              <YAxis />
-              <Tooltip
-                content={({ active, payload }) => {
-                  if (active && payload && payload.length) {
-                    return (
-                      <div className="rounded-lg border bg-background p-2 shadow-sm">
-                        <div className="grid grid-cols-2 gap-2">
-                          <div className="flex flex-col">
-                            <span className="text-[0.70rem] uppercase text-muted-foreground">
-                              Average
-                            </span>
-                            <span className="font-bold text-muted-foreground">
-                              {payload[0].value}
-                            </span>
+    <>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 pb-10">
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">No of users</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">{user}</div>
+            <p className="text-xs text-muted-foreground">
+              +20.1% from last month
+            </p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">
+              DDOS attack mode
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">{status}</div>
+          </CardContent>
+        </Card>
+      </div>
+      <Card>
+        <CardHeader>
+          <CardTitle>Network usage coverage</CardTitle>
+          <CardDescription>
+            Your exercise minutes are ahead of where you normally are.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="pb-4">
+          <div className="h-[200px]">
+            <ResponsiveContainer width="100%" height="100%">
+              <LineChart
+                data={data}
+                margin={{
+                  top: 5,
+                  right: 10,
+                  left: 10,
+                  bottom: 0,
+                }}
+              >
+                <XAxis dataKey="time" />
+                <YAxis />
+                <Tooltip
+                  content={({ active, payload }) => {
+                    if (active && payload && payload.length) {
+                      return (
+                        <div className="rounded-lg border bg-background p-2 shadow-sm">
+                          <div className="grid grid-cols-2 gap-2">
+                            <div className="flex flex-col">
+                              <span className="text-[0.70rem] uppercase text-muted-foreground">
+                                Average
+                              </span>
+                              <span className="font-bold text-muted-foreground">
+                                {payload[0].value}
+                              </span>
+                            </div>
                           </div>
                         </div>
-                      </div>
-                    );
-                  }
+                      );
+                    }
 
-                  return null;
-                }}
-              />
-              <Line
-                type="monotone"
-                strokeWidth={2}
-                dataKey="value"
-                activeDot={{
-                  r: 20,
-                  style: { fill: "var(--theme-primary)", opacity: 0.25 },
-                }}
-                style={
-                  {
-                    stroke: "var(--theme-primary)",
-                    opacity: 0.5,
-                    "--theme-primary": `hsl(${
-                      theme?.cssVars[mode === "dark" ? "light" : "dark"].primary
-                    })`,
-                  } as React.CSSProperties
-                }
-              />
-            </LineChart>
-          </ResponsiveContainer>
-        </div>
-      </CardContent>
-    </Card>
+                    return null;
+                  }}
+                />
+                <Line
+                  type="monotone"
+                  strokeWidth={2}
+                  dataKey="value"
+                  activeDot={{
+                    r: 20,
+                    style: { fill: "var(--theme-primary)", opacity: 0.25 },
+                  }}
+                  style={
+                    {
+                      stroke: "var(--theme-primary)",
+                      opacity: 0.5,
+                      "--theme-primary": `hsl(${
+                        theme?.cssVars[mode === "dark" ? "light" : "dark"]
+                          .primary
+                      })`,
+                    } as React.CSSProperties
+                  }
+                />
+              </LineChart>
+            </ResponsiveContainer>
+          </div>
+        </CardContent>
+      </Card>
+    </>
   );
 }
