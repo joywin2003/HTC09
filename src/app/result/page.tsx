94c8829815734823b9934d8b7dc82cfcc6baf881
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-02-21 08:53:36 +0530

    r

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -70,7 +70,7 @@ const ReportViewer: React.FC<ReportViewerProps> = () => {
 
         setLoader(true);
         const response = await fetch(
-          "https://6328-103-89-232-66.ngrok-free.app/net",
+          "https://1788-103-89-232-66.ngrok-free.app/LLM",
           requestOptions
         );
 
